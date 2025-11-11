7179e93bda00aa5f1ac23ff276b5dad3aba4b852
त्रिकालदर्शी <86564011+trikaaldarshi@users.noreply.github.com>
2025-11-11 13:06:43 +0530

    Fix formatting of LOG_LEVEL assignment

--- config_local.py
+++ config_local.py
@@ -1,6 +1,5 @@
 import os
 
-# Directly read from Heroku Config Vars
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID", "")
 UPDATE_INTERVAL = int(os.getenv("UPDATE_INTERVAL", 3600))
